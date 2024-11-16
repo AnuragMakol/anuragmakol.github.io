60ccc05bb05fec77fb3762163fbb4b1ac5b5edc4
ankushv-sdm <ankush.verma@smartdatainc.net>
2024-11-17 00:44:54 +0530

    widget page full height

--- src/components/sections.jsx
+++ src/components/sections.jsx
@@ -17,18 +17,8 @@ export const UserHeader = (props) => {
         <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1">
             <div className="flex flex-grow items-center justify-between px-4 py-2 shadow-2 md:px-6 2xl:px-11">
                 <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
-                    <button className="z-99999 block rounded-sm border border-stroke bg-white p-1.5 shadow-sm  lg:hidden">
-                        <span className="relative block h-5.5 w-5.5 cursor-pointer">
-                            <span className="du-block absolute right-0 h-full w-full">
-                                <span className="relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-[0] duration-200 ease-in-out "></span>
-                                <span className="relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-150 duration-200 ease-in-out "></span>
-                                <span className="relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-200 duration-200 ease-in-out "></span>
-                            </span>
-                            <span className="du-block absolute right-0 h-full w-full rotate-45">
-                                <span className="absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out "></span>
-                                <span className="delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out "></span>
-                            </span>
-                        </span>
+                    <button className="w-8 h-8 z-99999 block rounded-sm ring-1 ring-stroke bg-white p-1 shadow-sm  lg:hidden mr-2">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M 4 7 L 4 9 L 28 9 L 28 7 Z M 4 15 L 4 17 L 28 17 L 28 15 Z M 4 23 L 4 25 L 28 25 L 28 23 Z"/></svg>
                     </button>
                     <a className="block flex-shrink-0 lg:hidden" onClick={() => navigate('/dashboard')}>
                         <img src={`${import.meta.env.VITE_APP_URL}/images/logo/logo-icon.svg`} alt="Logo" />

21dbe8238d5dee22effc737059dc329805508956
‘AnuragMakol’ <anurag.makol@outlook.com>
2025-01-29 15:49:50 +0530

    Fixed Font Weight on Widget Page

--- src/pages/user/widget.jsx
+++ src/pages/user/widget.jsx
@@ -996,14 +996,33 @@ export function Widget(props) {
                                                 <div className="flex justify-between items-center mb-4">
                                                     <label className="block text-sm font-medium text-black">Font Weight</label>
                                                     <div className='min-w-32 max-w-32'>
-                                                        <input type='number' className='block-form-control h-9' {...registerUpdateWidgetTemplate('urgency_bar_text_font_weight')} onChange={(e) => {
-                                                            setWidgetSettings({
-                                                                ...widgetSettings,
-                                                                urgency_bar_text_font_weight: e.target.value
-                                                            });
+                                                        <div className="relative z-20 bg-input-color">
+                                                            <select className="block-select-control h-9" {...registerUpdateWidgetTemplate('urgency_bar_text_font_weight')} onChange={(e) => {
+                                                                setWidgetSettings({
+                                                                    ...widgetSettings,
+                                                                    urgency_bar_text_font_weight: e.target.value
+                                                                });
 
-                                                            manageCustomStyle('urgency_bar_text_font_weight', e.target.value);
-                                                        }} />
+                                                                manageCustomStyle('urgency_bar_text_font_weight', e.target.value);
+                                                            }}>
+                                                                <option value="100">100</option>
+                                                                <option value="200">200</option>
+                                                                <option value="300">300</option>
+                                                                <option value="400">400</option>
+                                                                <option value="500">500</option>
+                                                                <option value="600">600</option>
+                                                                <option value="700">700</option>
+                                                                <option value="800">800</option>
+                                                                <option value="900">900</option>
+                                                            </select>
+                                                            <span className="absolute right-2 top-1/2 z-10 -translate-y-1/2">
+                                                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                    <g opacity="0.5">
+                                                                        <path fillRule="evenodd" clipRule="evenodd" d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z" fill="#000"></path>
+                                                                    </g>
+                                                                </svg>
+                                                            </span>
+                                                        </div>
                                                     </div>
                                                 </div>
                                                 <div className="flex justify-between items-center mb-4">
@@ -1033,16 +1052,35 @@ export function Widget(props) {
                                                     </div>
                                                 </div>
                                                 <div className="flex justify-between items-center mb-4">
-                                                    <label className="block text-sm font-medium text-black">Countdown Font Weight</label>
+                                                    <label className="block text-sm font-medium text-black">Font Weight</label>
                                                     <div className='min-w-32 max-w-32'>
-                                                        <input type='number' className='block-form-control h-9' {...registerUpdateWidgetTemplate('urgency_bar_timer_font_weight')} onChange={(e) => {
-                                                            setWidgetSettings({
-                                                                ...widgetSettings,
-                                                                urgency_bar_timer_font_weight: e.target.value
-                                                            });
+                                                        <div className="relative z-20 bg-input-color">
+                                                            <select className="block-select-control h-9" {...registerUpdateWidgetTemplate('urgency_bar_timer_font_weight')} onChange={(e) => {
+                                                                setWidgetSettings({
+                                                                    ...widgetSettings,
+                                                                    urgency_bar_timer_font_weight: e.target.value
+                                                                });
 
-                                                            manageCustomStyle('urgency_bar_timer_font_weight', e.target.value);
-                                                        }} />
+                                                                manageCustomStyle('urgency_bar_timer_font_weight', e.target.value);
+                                                            }}>
+                                                                <option value="100">100</option>
+                                                                <option value="200">200</option>
+                                                                <option value="300">300</option>
+                                                                <option value="400">400</option>
+                                                                <option value="500">500</option>
+                                                                <option value="600">600</option>
+                                                                <option value="700">700</option>
+                                                                <option value="800">800</option>
+                                                                <option value="900">900</option>
+                                                            </select>
+                                                            <span className="absolute right-2 top-1/2 z-10 -translate-y-1/2">
+                                                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                    <g opacity="0.5">
+                                                                        <path fillRule="evenodd" clipRule="evenodd" d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z" fill="#000"></path>
+                                                                    </g>
+                                                                </svg>
+                                                            </span>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
